5a138dad65c0e6c00aa2d85d87996c39c787346e
Bianca Nobrega <bmoura@live.com>
2021-02-04 19:59:12 +0000

    desafio 1: criar model de usuario

---
+++ codepix/domain/model/user.go
@@ -0,0 +1,33 @@
+package model
+
+import (
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type User struct {
+	Base  `valid:"required"`
+	Name  string `json:"name" valid:"notnull"`
+	Email string `json:"email" valid:"notnull"`
+}
+
+func (user *User) isValid() error {
+	_, err := govalidator.ValidateStruct(user)
+	return err
+}
+
+func NewUser(name string, email string) (*User, error) {
+	user := User{
+		Name:  name,
+		Email: email,
+	}
+
+	user.ID = uuid.NewV4().String()
+
+	err := user.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
